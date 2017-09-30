b9772400446df272e93c53d546ec45d26e88dc39
Shaun T. Lim <stl51@composite.cs.rutgers.edu>
2017-09-30 16:58:20 -0400

    ordering variables

--- imdbproj.c
+++ imdbproj.c
@@ -5,34 +5,38 @@
 struct film{
 		char* color;
 		char* director_name;
-		char* movie_title;
 		int num_critic_for_reviews;
 		int duration;
 		int director_facebook_likes;
-		int actor_1_facebook_likes;
-		int actor_2_facebook_likes;
 		int actor_3_facebook_likes;
-		char* actor_1_name;
 		char* actor_2_name;
-		char* actor_3_name;
+		int actor_1_facebook_likes;
 		int gross;
 		char* genres;
+		char* actor_1_name;
+		char* movie_title;		
 		int num_voted_users;
 		int cast_total_facebook_likes;
+		char* actor_3_name;		
 		int facenumber_in_poster;
 		char* plot_keywords;
+		char* move_imdb_link;
 		int num_user_for_reviews;
 		char* language;
 		char* country;
 		char* content_rating;
-		double budget;
+		int budget;
 		int title_year;
+		int actor_2_facebook_likes;	
 		double imdb_score;
-		double aspect_ration;
+		double aspect_ratio;
 		int movie_facebook_likes;
-};
+}film;
 
 
 int main(int argc, char** argv){
+	
+	int sortby; //integer holding value corresponding to what the files will be sorted by
+	
 
 }
