f3e9507d6d2fb86d1258d8b141ec3a526fd87bbf
Shaun T. Lim <stl51@composite.cs.rutgers.edu>
2017-09-30 16:29:49 -0400

    first commit

---
+++ imdbproj.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct film{
+		char* color;
+		char* director_name;
+		char* movie_title;
+		int num_critic_for_reviews;
+		int duration;
+		int director_facebook_likes;
+		int actor_1_facebook_likes;
+		int actor_2_facebook_likes;
+		int actor_3_facebook_likes;
+		char* actor_1_name;
+		char* actor_2_name;
+		char* actor_3_name;
+		int gross;
+		char* genres;
+		int num_voted_users;
+		int cast_total_facebook_likes;
+		int facenumber_in_poster;
+		char* plot_keywords;
+		int num_user_for_reviews;
+		char* language;
+		char* country;
+		char* content_rating;
+		double budget;
+		int title_year;
+		double imdb_score;
+		double aspect_ration;
+		int movie_facebook_likes;
+};
+
+
+int main(int argc, char** argv){
+
+}
